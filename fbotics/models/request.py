0ea9bc64ad89da71dd755521fbf0a08ac4c695e4
Pashutan Modaresi <modaresi@qlaym.com>
2018-12-14 13:06:59 +0100

    Update request model to support tags

--- fbotics/models/request.py
+++ fbotics/models/request.py
@@ -8,6 +8,32 @@ from fbotics.models.recipient import Recipient
 
 # TODO: Add missing fields and constraints
 class Request(Model):
-    messaging_type = StringType(required=True, default="RESPONSE")
+    messaging_type = StringType(
+        required=True,
+        choices=[
+            "RESPONSE",
+            "MESSAGE_TAG",
+            "UPDATE"],
+        default="RESPONSE")
+    tag = StringType(
+        required=False,
+        choices=[
+            "BUSINESS_PRODUCTIVITY",
+            "COMMUNITY_ALERT",
+            "CONFIRMED_EVENT_REMINDER",
+            "NON_PROMOTIONAL_SUBSCRIPTION",
+            "PAIRING_UPDATE",
+            "APPLICATION_UPDATE",
+            "ACCOUNT_UPDATE",
+            "PAYMENT_UPDATE",
+            "PERSONAL_FINANCE_UPDATE",
+            "SHIPPING_UPDATE",
+            "RESERVATION_UPDATE",
+            "ISSUE_RESOLUTION",
+            "APPOINTMENT_UPDATE",
+            "GAME_EVENT",
+            "TRANSPORTATION_UPDATE",
+            "FEATURE_FUNCTIONALITY_UPDATE",
+            "TICKET_UPDATE"])
     recipient = ModelType(Recipient)
     message = ModelType(Message)
